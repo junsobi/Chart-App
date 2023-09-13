4529f99b4b504c5e15639e7add3be3348a26206a
junsobi <junsobi@gmail.com>
2023-09-13 11:38:39 +0900

    style : 레이아웃 조정

--- src/layouts/ChartLayout.tsx
+++ src/layouts/ChartLayout.tsx
@@ -7,7 +7,7 @@ interface ChartLayoutProps {
 const ChartLayout: React.FC<ChartLayoutProps> = ({ children }) => {
   return (
     <div className="w-screen h-screen flex items-center justify-center bg-gray-100">
-      <div className="w-5/6 flex flex-col gap-10">{children}</div>
+      <div className="w-5/6 flex flex-col">{children}</div>
     </div>
   );
 };
